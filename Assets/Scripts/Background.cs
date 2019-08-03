856867f1f705575b8d2982454035e7aeb4f9a108
Andrik Kleine <andrik.k@web.de>
2019-08-03 03:03:56 +0200

    Added level start in samplescene

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Camera.main.gameObject.transform.position.x, Camera.main.gameObject.transform.position.y, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Camera.main.gameObject.transform.position.x, Camera.main.gameObject.transform.position.y, 0.0f);
+    }
+}
---
+++ Assets/Scripts/ItemCrate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCrate : MonoBehaviour
+{
+	public GameObject player;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Debug.Log(col.gameObject.name);
+		if(GameObject.ReferenceEquals(col.gameObject, player))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
