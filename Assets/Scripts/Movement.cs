e60959bd468994ef91b0711ffc21b1cdc4662adb
Suritama <IchBinDeppert@outlook.com>
2019-08-03 15:03:40 +0200

    Vier Booleans! VIER! VAU I E ERgit pushgit push11Elf!

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,6 +12,12 @@ public class Movement : MonoBehaviour
 	
 	public bool doubleJump = false;
 	public bool fighting = false;
+	bool dJumpSwitch = false;
+	bool dJumpDelaySwitch = true;
+	bool dJumpDebugSwitch = true;
+	bool dJumpDelayDelay = true;
+	int dJumpDelay =10;
+	
 	
 	
 	
@@ -27,15 +33,53 @@ public class Movement : MonoBehaviour
 		hmove = (Input.GetAxisRaw("Horizontal") * speed); 
        if (Input.GetKey(KeyCode.Space)||Input.GetKey(KeyCode.UpArrow))
 	   {
-			animator.SetBool("IsJumping", true);
-			jump=true;
+			if(doubleJump)
+			{
+				dJumpDelayDelay = false;
+			}
+			if (doubleJump && dJumpSwitch && dJumpDebugSwitch)
+			{
+				dJumpSwitch=false;
+				animator.SetBool("IsJumping", false);
+				controller.m_Grounded = true;
+				controller.Move(hmove * Time.fixedDeltaTime, false, true);
+				dJumpDebugSwitch=false;
+			}
+			else
+			{
 			
+				animator.SetBool("IsJumping", true);
+				jump=true;
+				if(dJumpDelaySwitch)
+				{
+					dJumpSwitch=false;
+					dJumpDelaySwitch=false;
+					
+				}
+			
+			}
 	   }
+	   if (!dJumpDelaySwitch && dJumpDelay ==0 &&(Input.GetKeyUp(KeyCode.Space) ||Input.GetKeyUp(KeyCode.UpArrow))) 
+	   {
+			dJumpDelay=-1;			
+			dJumpSwitch=true;
+			
+	   
+		}	
 	   animator.SetFloat("HeroSpeed",Mathf.Abs(hmove));
+	   if (!dJumpDelayDelay&&dJumpDelay>0)
+	   {
+			dJumpDelay--;
+	   }
+	  
     }
 	
 	void FixedUpdate()
+	
 	{
+		  
+			
+	   
 		controller.Move(hmove * Time.fixedDeltaTime, false, jump);
 		
 		jump=false;
@@ -44,6 +88,10 @@ public class Movement : MonoBehaviour
 	public void OnLanding()
 	{
 		animator.SetBool("IsJumping", false);
+		dJumpDelaySwitch=true;
+		dJumpDebugSwitch = true;
+		dJumpDelayDelay = true;
+		dJumpDelay = 10;
 	}
 	
 	public void ChangeItem(string itemType)
