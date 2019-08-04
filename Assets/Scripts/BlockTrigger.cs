579c3c799d60c45f5f6b6655cfed4c6e38e6657f
Andrik Kleine <andrik.k@web.de>
2019-08-04 13:58:19 +0200

    yeetus

---
+++ Assets/Scripts/BlockTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTrigger : MonoBehaviour
+{
+	public GameObject faceblock;
+	
+	public GameObject player;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(GameObject.ReferenceEquals(col.gameObject, player))
+		{
+			faceblock.SendMessage("moveBlock");
+		}
+	}
+}
---
+++ Assets/Scripts/Faceblock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Faceblock : MonoBehaviour
+{
+	bool move = false;
+	
+	float origY;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        origY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move && origY - 3 < transform.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, 0);
+		}
+    }
+	
+	public void moveBlock()
+	{
+		move = true;
+	}
+}
